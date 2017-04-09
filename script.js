285864b0d64cacac4b7353af611c45162cad9006
Christian Briscoe <cbriscoe1995@gmail.com>
2017-04-09 14:21:41 -0400

    Fixed text display implementation, created a function that displays canned text then added seperate event listeners that update user inputs and add them to canned text by calling profile function

--- script.js
+++ script.js
@@ -2,90 +2,23 @@ var firstName = document.getElementById('first-name')
 var lastName = document.getElementById('last-name')
 var renderedText = document.getElementById('rendered-html')
 var rawText = document.getElementById('raw-html')
-var descriptionText = document.getElementById('description')
-var emailText = document.getElementById('email')
-var phoneText = document.getElementById('phone')
+var description = document.getElementById('description')
+var email = document.getElementById('email')
+var phone = document.getElementById('phone')
 
 function profile () {
-  // First name
-  firstName.addEventListener('input', function () {
-    var first = firstName.value
-    var last = lastName.value
-    var description = descriptionText.value
-    var email = emailText.value
-    var phone = phoneText.value
+  var profileText = '<h1>Hi, my name is ' + firstName.value + ' ' + lastName.value + '</h1>' + '<p>' +
+  description.value + '</p>' + 'if you\'re interested in a date, you can email me at ' + '<a href="mailto:' +
+  email.value + '" target="_blank">' + email.value + '</a>' + ' or give me a call at ' + '<a href="tel:' +
+  phone.value + '" target="_blank">' + phone.value + '</a>'
 
-    renderedText.innerHTML = '<h1>Hi, my name is ' + first + ' ' + last + '</h1>' + '<p>' + description + '</p>' +
-    'if you\'re interested in a date, you can email me at ' + '<a href="mailto:' + email + '" target="_blank">' +
-    email + '</a>' + ' or give me a call at ' + '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>'
-
-    rawText.textContent = '<h1>Hi, my name is ' + first + ' ' + last + '</h1>' + '<p>' + description + '</p>' +
-    'if you\'re interested in a date, you can email me at ' + '<a href="mailto:' + email + '"target="_blank">' +
-    email + '</a>' + ' or give me a call at ' + '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>'
-  })
-  // Last Name
-  lastName.addEventListener('input', function () {
-    var first = firstName.value
-    var last = lastName.value
-    var description = descriptionText.value
-    var email = emailText.value
-    var phone = phoneText.value
-
-    renderedText.innerHTML = '<h1>Hi, my name is ' + first + ' ' + last + '</h1>' + '<p>' + description + '</p>' +
-    'if you\'re interested in a date, you can email me at ' + '<a href="mailto:' + email + '"target="_blank">' +
-    email + '</a>' + ' or give me a call at ' + '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>'
-
-    rawText.textContent = '<h1>Hi, my name is ' + first + ' ' + last + '</h1>' + '<p>' + description + '</p>' +
-    'if you\'re interested in a date, you can email me at ' + '<a href="mailto:' + email + '"target="_blank">' +
-    email + '</a>' + ' or give me a call at ' + '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>'
-  })
-  //  Description
-  descriptionText.addEventListener('input', function () {
-    var first = firstName.value
-    var last = lastName.value
-    var description = descriptionText.value
-    var email = emailText.value
-    var phone = phoneText.value
-
-    renderedText.innerHTML = '<h1>Hi, my name is ' + first + ' ' + last + '</h1>' + '<p>' + description + '</p>' +
-    'if you\'re interested in a date, you can email me at ' + '<a href="mailto:' + email + '"target="_blank">' +
-    email + '</a>' + ' or give me a call at ' + '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>'
-
-    rawText.textContent = '<h1>Hi, my name is ' + first + ' ' + last + '</h1>' + '<p>' + description + '</p>' +
-    'if you\'re interested in a date, you can email me at ' + '<a href="mailto:' + email + '"target="_blank">' +
-    email + '</a>' + ' or give me a call at ' + '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>'
-  })
-  //  Email
-  emailText.addEventListener('input', function () {
-    var first = firstName.value
-    var last = lastName.value
-    var description = descriptionText.value
-    var email = emailText.value
-    var phone = phoneText.value
-
-    renderedText.innerHTML = '<h1>Hi, my name is ' + first + ' ' + last + '</h1>' + '<p>' + description + '</p>' +
-    'if you\'re interested in a date, you can email me at ' + '<a href="mailto:' + email + '"target="_blank">' +
-    email + '</a>' + ' or give me a call at ' + '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>'
-
-    rawText.textContent = '<h1>Hi, my name is ' + first + ' ' + last + '</h1>' + '<p>' + description + '</p>' +
-    'if you\'re interested in a date, you can email me at ' + '<a href="mailto:' + email + '"target="_blank">' +
-    email + '</a>' + ' or give me a call at ' + '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>'
-  })
-  //  Phone
-  phoneText.addEventListener('input', function () {
-    var first = firstName.value
-    var last = lastName.value
-    var description = descriptionText.value
-    var email = emailText.value
-    var phone = phoneText.value
-
-    renderedText.innerHTML = '<h1>Hi, my name is ' + first + ' ' + last + '</h1>' + '<p>' + description + '</p>' +
-    'if you\'re interested in a date, you can email me at ' + '<a href="mailto:' + email + '"target="_blank">' +
-    email + '</a>' + ' or give me a call at ' + '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>'
-
-    rawText.textContent = '<h1>Hi, my name is ' + first + ' ' + last + '</h1>' + '<p>' + description + '</p>' +
-    'if you\'re interested in a date, you can email me at ' + '<a href="mailto:' + email + '"target="_blank">' +
-    email + '</a>' + ' or give me a call at ' + '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>'
-  })
+  renderedText.innerHTML = profileText
+  rawText.textContent = profileText
 }
-profile()
+
+// Event Listeners
+firstName.addEventListener('input', profile)
+lastName.addEventListener('input', profile)
+description.addEventListener('input', profile)
+email.addEventListener('input', profile)
+phone.addEventListener('input', profile)
